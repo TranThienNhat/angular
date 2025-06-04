5e20c1b91f3bc0fc13f47874d80c06678f104147
hoangv1et <nonghoangviet30121995@gmail.com>
2025-06-05 00:40:09 +0700

    add SP in home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ProductServiceService } from '../service/product.service';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  products: any[] = [];
+
+  constructor(
+    private productService: ProductServiceService,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const Id = Number(params.get('Id'));
+      this.productService.getProduct(Id).subscribe((data) => {
+        this.products = data;
+      });
+    });
+  }
+}
